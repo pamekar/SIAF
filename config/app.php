a703a9aa67f4137fcc6460f10a06c7f810c93081
GreenWhite <greenwhitedev@gmail.com>
2019-04-21 02:39:27 +0100

    No need for $public prefix on cloud hosting

--- config/app.php
+++ config/app.php
@@ -14,6 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
+    'shared_hosting' => env('SHARED_HOSTING', false),
 
     /*
     |--------------------------------------------------------------------------
