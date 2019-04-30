4e5938b0aa6b0bd4bdc07895c987db7a4093dfd8
GreenWhite <greenwhitedev@gmail.com>
2019-04-30 17:25:44 +0100

    Create View for Posts

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ if (!config('app.isDemo')) {
         function () {
             Route::get('/', 'HomeController@index');
             Route::get('/home', 'HomeController@index')->name('home');
+            Route::post('/contact', 'HomeController@contact')->name('contact');
             Route::get('/donate', 'DonateController@index')->name('donate');
         });
 } else {
