2e8920126f5febe975758e14985178b441e8dab2
GreenWhite <greenwhitedev@gmail.com>
2019-05-14 16:27:29 +0100

    API routes

--- routes/api.php
+++ routes/api.php
@@ -17,13 +17,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/', 'HomeController@index')->name('home');
+
 Route::group([
 
     'middleware' => 'api',
     'prefix' => 'auth'
 
 ], function ($router) {
-
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
