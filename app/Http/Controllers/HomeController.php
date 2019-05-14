8e2187f115c988fd6101296047681517dba15abe
GreenWhite <greenwhitedev@gmail.com>
2019-05-14 16:25:52 +0100

    Provide data for frontend

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Complaint;
+use App\Models\Contribution;
+use App\Models\Expense;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
-    public function index(){
-        return view('dashboard');
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $resolved = Complaint::where('status', 'resolved')->count();
+        $pending = Complaint::where('status', 'pending')->count();
+        $contributions = Contribution::sum('amount') / 100;
+        $expenses = Expense::sum('amount') / 100;
+        return response()->json(compact('resolved', 'pending', 'contributions',
+            'expenses'));
     }
+
 }
