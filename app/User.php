c8d9d13b7aabe4f2f5fc30950e9ff620e212aa27
GreenWhite <greenwhitedev@gmail.com>
2019-05-18 07:56:45 +0100

    Serve dashboard data via accessors on model

--- app/User.php
+++ app/User.php
@@ -2,9 +2,14 @@
 
 namespace App;
 
+use App\Models\Complaint;
+use App\Models\Contribution;
+use App\Models\Expense;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Redis;
+use TCG\Voyager\Facades\Voyager;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends \TCG\Voyager\Models\User implements JWTSubject
@@ -16,27 +21,92 @@ class User extends \TCG\Voyager\Models\User implements JWTSubject
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $fillable
+        = [
+            'name',
+            'email',
+            'password',
+        ];
 
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $hidden
+        = [
+            'password',
+            'remember_token',
+        ];
 
     /**
      * The attributes that should be cast to native types.
      *
      * @var array
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    protected $casts
+        = [
+            'email_verified_at' => 'datetime',
+        ];
+
+    public function getAvatarAttribute($value)
+    {
+        return Voyager::image($value);
+    }
+
+    public function getContributionsAttribute()
+    {
+        if ($contributions = Redis::get('dashboard.contributions')) {
+            return $contributions;
+        }
+
+        $contributions = Contribution::sum('amount')/100;
+
+        Redis::setex('dashboard.contributions', 60 * 60 * 24, $contributions);
+
+        return $contributions;
+    }
+
+
+    public function getExpensesAttribute()
+    {
+        if ($expenses = Redis::get('dashboard.expenses')) {
+            return $expenses;
+        }
+
+        $expenses = $expenses = Expense::sum('amount') / 100;
+
+        Redis::setex('dashboard.expenses', 60 * 60 * 24, $expenses);
+
+        return $expenses;
+    }
+
+
+    public function getPendingAttribute()
+    {
+        if ($pending = Redis::get('dashboard.pending')) {
+            return $pending;
+        }
+
+        $pending = Complaint::where('status', 'pending')->count();
+
+        Redis::setex('dashboard.pending', 60 * 60 * 24, $pending);
+
+        return $pending;
+    }
+
+    public function getResolvedAttribute()
+    {
+        if ($resolved = Redis::get('dashboard.resolved')) {
+            return $resolved;
+        }
+
+        $resolved = Complaint::where('status', 'resolved')->count();
+
+        Redis::setex('dashboard.resolved', 60 * 60 * 24, $resolved);
+
+        return $resolved;
+    }
 
     public function getJWTIdentifier()
     {
