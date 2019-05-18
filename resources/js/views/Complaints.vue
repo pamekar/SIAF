12ccdf1df63c01b33fcafe2962dc48abf068dd3c
GreenWhite <greenwhitedev@gmail.com>
2019-05-18 08:03:26 +0100

    Application views

---
+++ resources/js/views/Complaints.vue
@@ -0,0 +1,30 @@
+<template>
+    <main id="main-container">
+
+    </main>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                dashboard: {}
+            };
+        },
+
+        created() {
+
+        },
+        
+        methods:    {
+
+        },
+        mounted() {
+            
+        },
+        components: {},
+        computed:   {
+
+        }
+    }
+</script>
