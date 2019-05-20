b4a5b3a63f6806bd79123f25fc826fc2736fa94d
GreenWhite <greenwhitedev@gmail.com>
2019-05-20 13:07:46 +0100

    add router links and check isAdmin

--- resources/js/components/SidebarComponent.vue
+++ resources/js/components/SidebarComponent.vue
@@ -1,5 +1,4 @@
 <template>
-
     <nav id="sidebar" aria-label="Main Navigation">
         <div class="content-header bg-white-5">
             <a class="font-w600 text-dual" href="/">
@@ -12,27 +11,33 @@
         <div class="content-side content-side-full">
             <ul class="nav-main">
                 <li class="nav-main-item">
-                    <a class="nav-main-link active" href="/">
+                    <router-link class="nav-main-link disabled" :to="{name:'dashboard'}">
                         <i class="nav-main-link-icon si si-speedometer"></i>
                         <span class="nav-main-link-name">Dashboard</span>
+                    </router-link>
+                </li>
+                <li class="nav-main-item" v-if="user.is_admin">
+                    <a class="nav-main-link disabled" href="/admin">
+                        <i class="nav-main-link-icon si si-speedometer"></i>
+                        <span class="nav-main-link-name">Admin</span>
                     </a>
                 </li>
                 <li class="nav-main-heading">Complaints</li>
                 <li class="nav-main-item">
-                    <a class="nav-main-link active" href="/">
+                    <a class="nav-main-link disabled" href="/">
                         <i class="nav-main-link-icon si si-speedometer"></i>
                         <span class="nav-main-link-name">View Reports</span>
                     </a>
                 </li>
                 <li class="nav-main-item">
-                    <a class="nav-main-link active" href="/">
+                    <a class="nav-main-link disabled" href="/">
                         <i class="nav-main-link-icon si si-speedometer"></i>
                         <span class="nav-main-link-name">Report an Issue</span>
                     </a>
                 </li>
                 <li class="nav-main-heading">Contributions</li>
                 <li class="nav-main-item">
-                    <a class="nav-main-link active" href="/">
+                    <a class="nav-main-link disabled" href="/">
                         <i class="nav-main-link-icon si si-speedometer"></i>
                         <span class="nav-main-link-name">Contribute</span>
                     </a>
@@ -40,10 +45,25 @@
 
                 </li>
                 <li class="nav-main-item">
-                    <a class="nav-main-link active" href="/">
+                    <a class="nav-main-link disabled" href="/">
                         <i class="nav-main-link-icon si si-speedometer"></i>
                         <span class="nav-main-link-name">View Contributions</span>
                     </a>
+                </li>
+                <li class="nav-main-heading">User</li>
+                <li class="nav-main-item">
+                    <a class="nav-main-link disabled" href="/">
+                        <i class="nav-main-link-icon si si-speedometer"></i>
+                        <span class="nav-main-link-name">Profile</span>
+                    </a>
+
+
+                </li>
+                <li class="nav-main-item">
+                    <router-link class="nav-main-link disabled" :to="{name:'settings'}">
+                        <i class="nav-main-link-icon si si-speedometer"></i>
+                        <span class="nav-main-link-name">Settings</span>
+                    </router-link>
                 </li>
             </ul>
         </div>
@@ -51,15 +71,18 @@
 </template>
 
 <script>
+    import {queries} from '../queries'
 
     export default {
         data() {
-            return{
-                user:{
-                    name:"",
-                    avatar:""
+            return {
+                user: {
+                    is_admin: 0
                 }
             }
+        },
+        apollo: {
+            user: queries.checkAdmin,
         },
         props: {
             menu: Array
