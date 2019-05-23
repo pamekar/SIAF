18dd8c4674094a537f2849fd17aff0603ec004a5
GreenWhite <greenwhitedev@gmail.com>
2019-05-24 00:56:49 +0100

    New Page

--- resources/js/views/Members.vue
+++ resources/js/views/Members.vue
@@ -1,30 +1,52 @@
 <template>
     <main id="main-container">
-
+        <div class="bg-body-light">
+            <div class="content content-full">
+                <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
+                    <h1 class="flex-sm-fill h3 my-2">
+                        Form Layouts
+                        <small class="d-block d-sm-inline-block mt-2 mt-sm-0 font-size-base font-w400 text-muted">Multiple layouts to match any design requirement.</small>
+                    </h1>
+                    <nav class="flex-sm-00-auto ml-sm-3" aria-label="breadcrumb">
+                        <ol class="breadcrumb breadcrumb-alt">
+                            <li class="breadcrumb-item">Forms</li>
+                            <li class="breadcrumb-item" aria-current="page">
+                                <a class="link-fx" href="#">Layouts</a>
+                            </li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <div class="content">
+            <div class="block">
+                <div class="block-header">
+                    <h3 class="block-title">Inline</h3>
+                </div>
+                <div class="block-content block-content-full">
+                    <div class="row">
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
 <script>
     export default {
         data() {
-            return {
-                dashboard: {}
-            };
+            return {};
         },
 
         created() {
 
         },
-        
-        methods:    {
 
-        },
+        methods:    {},
         mounted() {
-            
+
         },
         components: {},
-        computed:   {
-
-        }
+        computed:   {}
     }
 </script>
