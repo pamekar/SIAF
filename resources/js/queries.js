f44922ff609c5ab65a296367dce89e66a0fda601
GreenWhite <greenwhitedev@gmail.com>
2019-05-24 00:59:24 +0100

    Create new user type queries

--- resources/js/queries.js
+++ resources/js/queries.js
@@ -1,12 +1,10 @@
 import gql from 'graphql-tag';
 
 export const queries = {
-    user: gql`query {
+    checkAdmin: gql`query {
         user {
             id
-            name
-            email
-            avatar
+            is_admin
         }
     }`,
     dashboard: gql`query {
@@ -17,6 +15,31 @@ export const queries = {
             expenses
             pending
             resolved
+        }
+    }`,
+    settings: gql`query {
+        user {
+            id
+            name
+            first_name
+            last_name
+            email
+            phone_no
+            linkedin
+            instagram
+            facebook
+            twitter
+            avatar
+            about
+        }
+    }`,
+    user: gql`query {
+        user {
+            id
+            name
+            email
+            avatar
+            is_admin
         }
     }`
 };
