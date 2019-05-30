d61f5a1a72887ca9d1c5b3f78b2a48d600b15a53
GreenWhite <greenwhitedev@gmail.com>
2019-05-30 21:42:37 +0100

    Replace assets linking

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -13,12 +13,17 @@
     <meta property="og:type" content="website">
     <meta property="og:url" content="">
     <meta property="og:image" content="">
-    <link rel="shortcut icon" href="{{$public}}/png/favicon.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="{{$public}}/png/favicon-192x192.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{$public}}/png/apple-touch-icon-180x180.png">
-    <link rel="stylesheet" href="{{$public}}/css/css1969.css?family=Source+Sans+Pro:300,400,400italic,600,700%7COpen+Sans:300,400,400italic,600,700">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{$public}}/png/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{$public}}/png/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{$public}}/png/favicon-16x16.png">
+    <link rel="manifest" href="{{$public}}/site.webmanifest">
+    <link rel="mask-icon" href="{{$public}}/svg/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet"
+          href="{{$public}}/css/css1969.css?family=Source+Sans+Pro:300,400,400italic,600,700%7COpen+Sans:300,400,400italic,600,700">
     <link rel="stylesheet" id="css-main" href="{{$public}}/css/oneui.min-4.2.css">
-    <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','js/analytics.js','ga');ga('create', 'UA-16158021-6', 'auto');ga('send', 'pageview');</script>
+
 </head>
 <body>
 <div id="page-container">
