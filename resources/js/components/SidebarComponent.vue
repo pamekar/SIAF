c19a580eb045b8ec8009d8f1c9334479688dcf66
GreenWhite <greenwhitedev@gmail.com>
2019-05-30 12:02:02 +0100

    Set icons for links

--- resources/js/components/SidebarComponent.vue
+++ resources/js/components/SidebarComponent.vue
@@ -2,9 +2,9 @@
     <nav id="sidebar" aria-label="Main Navigation">
         <div class="content-header bg-white-5">
             <a class="font-w600 text-dual" href="/">
-                <i class="fa fa-circle-notch text-primary"></i>
-                <span class="smini-hide">
-                <span class="font-w700 font-size-h5">ne</span> <span class="font-w400">4.2</span>
+                <img :src="public_+'/png/favicon-32x32.png'"></img>
+                <span class="smini-hide pl-3">
+                <span class="font-w700 font-size-h3"> SIAF</span> <span class="font-w400">NG</span>
             </span>
             </a>
         </div>
@@ -18,27 +18,27 @@
                 </li>
                 <li class="nav-main-item" v-if="user.is_admin">
                     <a class="nav-main-link disabled" href="/admin">
-                        <i class="nav-main-link-icon si si-speedometer"></i>
+                        <i class="nav-main-link-icon si si-wrench"></i>
                         <span class="nav-main-link-name">Admin</span>
                     </a>
                 </li>
                 <li class="nav-main-heading">Complaints</li>
                 <li class="nav-main-item">
                     <a class="nav-main-link disabled" href="/users/complaints">
-                        <i class="nav-main-link-icon si si-speedometer"></i>
+                        <i class="nav-main-link-icon si si-book-open"></i>
                         <span class="nav-main-link-name">View Reports</span>
                     </a>
                 </li>
                 <li class="nav-main-item">
                     <a class="nav-main-link disabled" href="/users/report">
-                        <i class="nav-main-link-icon si si-speedometer"></i>
+                        <i class="nav-main-link-icon si si-pencil"></i>
                         <span class="nav-main-link-name">Report an Issue</span>
                     </a>
                 </li>
                 <li class="nav-main-heading">Contributions</li>
                 <li class="nav-main-item">
                     <a class="nav-main-link disabled" href="/users/contribute">
-                        <i class="nav-main-link-icon si si-speedometer"></i>
+                        <i class="nav-main-link-icon si si-heart"></i>
                         <span class="nav-main-link-name">Contribute</span>
                     </a>
 
@@ -46,20 +46,20 @@
                 </li>
                 <li class="nav-main-item">
                     <a class="nav-main-link disabled" href="/users/contributions">
-                        <i class="nav-main-link-icon si si-speedometer"></i>
+                        <i class="nav-main-link-icon si si-wallet"></i>
                         <span class="nav-main-link-name">View Contributions</span>
                     </a>
                 </li>
-                <li class="nav-main-heading">User</li>
+                <li class="nav-main-heading">User Options</li>
                 <li class="nav-main-item">
                     <router-link class="nav-main-link disabled" :to="{name:'profile'}">
-                        <i class="nav-main-link-icon si si-speedometer"></i>
+                        <i class="nav-main-link-icon si si-user"></i>
                         <span class="nav-main-link-name">Profile</span>
                     </router-link>
                 </li>
                 <li class="nav-main-item">
-                    <router-link class="nav-main-link disabled" :to="{name:'settings'}">
-                        <i class="nav-main-link-icon si si-speedometer"></i>
+                    <router-link class="nav-main-link" :to="{name:'settings'}">
+                        <i class="nav-main-link-icon si si-settings"></i>
                         <span class="nav-main-link-name">Settings</span>
                     </router-link>
                 </li>
@@ -89,7 +89,7 @@
         apollo: {
             user: queries.checkAdmin,
         },
-        props: {
+        props:  {
             menu: Array
         }
     }
