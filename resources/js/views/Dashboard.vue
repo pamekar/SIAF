d9b0ecf5b378c162876e38defe3aca833ee9ac08
GreenWhite <greenwhitedev@gmail.com>
2019-05-30 09:28:22 +0100

    Comment graphs on dashboard

--- resources/js/views/Dashboard.vue
+++ resources/js/views/Dashboard.vue
@@ -55,7 +55,7 @@
                 </div>
             </div>
 
-            <div class="row">
+            <!--<div class="row">
                 <div class="col-lg-6">
                     <div class="block block-rounded block-mode-loading-oneui">
                         <div class="block-header">
@@ -136,7 +136,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>-->
         </div>
     </main>
 </template>
