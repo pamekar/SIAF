5ba90ef6afab268610d6ab30ad5b13e13a4223a5
GreenWhite <greenwhitedev@gmail.com>
2019-07-29 22:36:54 +0100

    Add summary and updated_on attribute

--- app/Models/Complaint.php
+++ app/Models/Complaint.php
@@ -6,6 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Complaint extends Model
 {
+    /**
+     * Attribute summarises complaint description
+     *
+     * @return bool|string
+     */
+    public function getSummaryAttribute()
+    {
+        $description = strip_tags($this->description);
 
-    //
+        return substr($description, 0, 300);
+    }
+
+    /**
+     * Gets a formatted date of when the complaint was last updated
+     *
+     * @return false|string
+     */
+    public function getUpdatedOnAttribute(){
+        return date_format($this->updated_at, config('app.date_format'));
+    }
 }
